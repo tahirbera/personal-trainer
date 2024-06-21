f8375b141d87fe66544f67d7a546fa97456d32a5
tahirbera <tahirberakeskin@gmail.com>
2024-06-22 01:28:21 +0300

    last-changes

--- src/components/Hizmetler.js
+++ src/components/Hizmetler.js
@@ -7,15 +7,15 @@ const Hizmetler = () => {
             <h2> <span style={{color: 'yellow'}}>İLETİŞİM  </span>
             ADRESLERİ</h2>
             <ul>
-                <li ><p className='hizmetler-li'>Telefon Et: <a href=''>0XXX xxx xxx</a></p></li>
-                <li ><p className='hizmetler-li'>WhatsApp: <a href="https://wa.me/+905066424651?text=Merhaba%20PT%20hizmeti%20hakkında%20bilgi%20almak%20istiyorum.">0XXX xxx xxx</a></p></li>
-                <li ><p className='hizmetler-li'>Instagram:<a href=''> sariselim</a></p></li>
+                <li ><p className='hizmetler-li'>Telefon Et: <a href='tel:+905066424651'>+90 506 642 4651</a></p></li>
+                <li ><p className='hizmetler-li'>WhatsApp: <a href="https://wa.me/+905066424651?text=Merhaba%20PT%20hizmeti%20hakkında%20bilgi%20almak%20istiyorum.">+90 506 642 4651</a></p></li>
+                <li ><p className='hizmetler-li'>Instagram:<a href='https://www.instagram.com/selim.sari91/'> selim.sarı91</a></p></li>
             </ul>
         </div>
         <div className='hizmetler-right'>
         <h2> <span style={{color: 'yellow'}}>CARBON GYM  </span>
             ADRES</h2>
-        <p>Manisa/ Uncubozköy Mahallesi / 5511 sokak</p>
+        <p>Uncubozköy, 5537. Sk. 32/A, 45010 Yunusemre/Manisa</p>
         </div>
        
     </div>
