b0e66de0fdcab3e6da213a06213724b24d682eda
tahirbera <tahirberakeskin@gmail.com>
2024-06-22 01:25:28 +0300

    asdf

--- src/components/Hizmetler.js
+++ src/components/Hizmetler.js
@@ -8,7 +8,7 @@ const Hizmetler = () => {
             ADRESLERİ</h2>
             <ul>
                 <li ><p className='hizmetler-li'>Telefon Et: <a href=''>0XXX xxx xxx</a></p></li>
-                <li ><p className='hizmetler-li'>WhatsApp: <a href="https://wa.me/+905066424651?text=Hello%20there!">0XXX xxx xxx</a></p></li>
+                <li ><p className='hizmetler-li'>WhatsApp: <a href="https://wa.me/+905066424651?text=Merhaba%20PT%20hizmeti%20hakkında%20bilgi%20almak%20istiyorum.">0XXX xxx xxx</a></p></li>
                 <li ><p className='hizmetler-li'>Instagram:<a href=''> sariselim</a></p></li>
             </ul>
         </div>
